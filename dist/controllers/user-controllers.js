b21e19e20c0a23913cad83c0b4d22903ddc5b9c9
paragborkar <paragborkar200@gmail.com>
2023-10-21 11:20:28 +0530

    Login Routes And Controllers Created

--- dist/controllers/user-controllers.js
+++ dist/controllers/user-controllers.js
@@ -1,9 +1,9 @@
 import ChatUser from "../models/ChatUser.js";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await ChatUser.find();
-        return res.status(200).json({ message: "OK", users });
+        return res.status(201).json({ message: "OK", users });
     }
     catch (error) {
         console.log(error);
@@ -24,6 +24,26 @@ export const userSignup = async (req, res, next) => {
         expires.setDate(expires.getDate() + 7);
         return res
             .status(201)
+            .json({ message: "OK", name: user.name, email: user.email });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await ChatUser.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+        return res
+            .status(200)
             .json({ message: "OK", name: user.name, email: user.email });
     }
     catch (error) {
--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -5,7 +5,7 @@ import { compare, hash} from "bcrypt";
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) =>{
     try {
         const users= await ChatUser.find();
-        return res.status(200).json({message:"OK", users})
+        return res.status(201).json({message:"OK", users})
     } catch (error) {
         console.log(error);
         return res.status(404).json({message: "ERROR", cause:error.message})
@@ -41,3 +41,33 @@ export const userSignup = async (
       return res.status(404).json({ message: "ERROR", cause: error.message });
     }
   };
+
+
+  export const userLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const {email, password } = req.body;
+     const user = await ChatUser.findOne({email})
+     if(!user)
+     {
+        return res.status(401).send("User not registered");
+     }
+     const isPasswordCorrect = await compare(password,user.password);
+     if(!isPasswordCorrect)
+     {
+        return res.status(403).send("Incorrect Password");
+     }
+      return res
+        .status(200)
+        .json({ message: "OK", name: user.name, email: user.email });
+    } catch (error) {
+      console.log(error);
+      return res.status(404).json({ message: "ERROR", cause: error.message });
+    }
+  };
+
+
+
