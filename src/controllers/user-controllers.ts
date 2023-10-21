fed8cbbcff6267e3178571cdab85b0e90dd52915
paragborkar <paragborkar200gmail.com>
2023-10-21 09:26:44 +0530

    Login Controller Removed

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -43,35 +43,4 @@ export const userSignup = async (
   };
 
 
-  export const userLogin = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const { email, password } = req.body;
-      const user = await ChatUser.findOne({ email });
-      if (!user) {
-        return res.status(401).send("User not registered");
-      }
-      const isPasswordCorrect = await compare(password, user.password);
-      if (!isPasswordCorrect) {
-        return res.status(403).send("Incorrect Password");
-      }
   
-      
-  
-     
-  
-      const expires = new Date();
-      expires.setDate(expires.getDate() + 7);
-      
-  
-      return res
-        .status(200)
-        .json({ message: "OK", name: user.name, email: user.email });
-    } catch (error) {
-      console.log(error);
-      return res.status(200).json({ message: "ERROR", cause: error.message });
-    }
-  };
